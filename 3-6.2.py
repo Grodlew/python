54d9f189ecaf2ef2d7ebdc62101a7b8d06d1e8ac
Grodlew <76842330+Grodlew@users.noreply.github.com>
2021-01-25 15:46:57 +0300

    Update 3-6.2.py
    Переделал в одну функцию без ord() и chr()

--- 3-6.2.py
+++ 3-6.2.py
@@ -1,20 +1,6 @@
-def int_func(word):
-    """
-    Делаем из слова список, узнаем номер первой буквы в Юникод и вычитаем 32,
-    чтобы получить такую же заглавную букву, которую вставляем на место строчной.
-    """
-    temp_list = list(word)
-    n = ord(temp_list[0]) - 32
-    temp_list[0] = chr(n)
-    return ''.join(temp_list)
-
-
-def int_func_x(words):
-    temp_list = words.split()
-    for i in range(len(temp_list)):
-        temp_list[i] = int_func(temp_list[i])
-    return ' '.join(temp_list)
-
-
-print(int_func('spam'))
-print(int_func_x('some random text'))
+def int_func(user_inp):
+  user_inp = ' '.join(word[0].upper() + word[1:] for word in user_inp.split())
+  return user_inp
+  
+  
+print(int_func(input('Введите одно или несколько слов строчными буквами: ')))
